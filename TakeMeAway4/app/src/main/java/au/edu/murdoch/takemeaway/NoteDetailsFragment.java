50357db3d1322bcd7b7aba87725fd8f4208f4e73
Jeremy Soh <jeremy@whooshpro.com>
2020-04-01 10:51:16 +0800

    By Jeremy ------------ Permission Changes: Removed Request for Internet
    ------------
    UI Changes:
    Changed Location from EditText to TextView
    
    
    ------------
    Code Changes:
    Added Location features
    - Check for location permission at start (New Note)
    - Check for location changes and update (New Note)
    - Clicking on location TextView prompt users for a location update
    
    -----------
    TODO:
    - Searching list
    - Saving
    - DB normalisation -> checking on delete function as well

--- TakeMeAway4/app/src/main/java/au/edu/murdoch/takemeaway/NoteDetailsFragment.java
+++ TakeMeAway4/app/src/main/java/au/edu/murdoch/takemeaway/NoteDetailsFragment.java
@@ -1,5 +1,6 @@
 package au.edu.murdoch.takemeaway;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.DatePickerDialog;
@@ -10,13 +11,22 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
@@ -30,17 +40,25 @@ import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.Toolbar;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import static android.content.Context.LOCATION_SERVICE;
+
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link NoteDetailsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
 public class NoteDetailsFragment extends Fragment {
-    private TakeMeAwayDBHelper mydb ;
+    private TakeMeAwayDBHelper mydb;
     static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final int GALLERY_REQUEST_CODE = 100;
 
@@ -53,6 +71,8 @@ public class NoteDetailsFragment extends Fragment {
     final Calendar myCalendar = Calendar.getInstance();
     private int noteId = 0;
     PopupWindow puwPhotoChoice;
+    LocationManager locationManager;
+    LocationListener locationListener;
 
     // the buttons
     FloatingActionButton mSaveButton;
@@ -62,6 +82,7 @@ public class NoteDetailsFragment extends Fragment {
     public NoteDetailsFragment() {
         // Required empty public constructor
     }
+
     /**
      * Create a new instance of DetailsFragment, initialized to
      * show the text at 'id'.
@@ -88,8 +109,9 @@ public class NoteDetailsFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setHasOptionsMenu(true);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -120,9 +142,9 @@ public class NoteDetailsFragment extends Fragment {
         timeTextView = mLayoutView.findViewById(R.id.textClock);
         int hour = myCalendar.get(Calendar.HOUR_OF_DAY);
         int minute = myCalendar.get(Calendar.MINUTE);
-        String hourStr = hour<10? "0"+hour : ""+hour;
-        String minuteStr = minute<10? "0"+minute : ""+minute;
-        timeTextView.setText(hourStr+":"+minuteStr);
+        String hourStr = hour < 10 ? "0" + hour : "" + hour;
+        String minuteStr = minute < 10 ? "0" + minute : "" + minute;
+        timeTextView.setText(hourStr + ":" + minuteStr);
 
         return mLayoutView;
     }
@@ -130,18 +152,18 @@ public class NoteDetailsFragment extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        ((AppCompatActivity)getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         if (mLayoutView == null)
             return;
-        title   = mLayoutView.findViewById(R.id.title);
-        content  = mLayoutView.findViewById(R.id.content);
+        title = mLayoutView.findViewById(R.id.title);
+        content = mLayoutView.findViewById(R.id.content);
         location = mLayoutView.findViewById(R.id.location);
         dateTextView = mLayoutView.findViewById(R.id.dateTime);
         timeTextView = mLayoutView.findViewById(R.id.textClock);
         mydb = new TakeMeAwayDBHelper(getActivity());
 
 
-        mSaveButton =  mLayoutView.findViewById(R.id.button_save);
+        mSaveButton = mLayoutView.findViewById(R.id.button_save);
 
         mChangePhoto = mLayoutView.findViewById(R.id.TakePhoto);
         imvBanner = mLayoutView.findViewById(R.id.newImage);
@@ -150,88 +172,197 @@ public class NoteDetailsFragment extends Fragment {
         //toolbar = mLayoutView.findViewById(R.id.toolbar);
         //toolbar.setNavigationIcon(R.drawable.ic_delete_black_24dp);
 
+        //location
+        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+
+            @Override
+            public void onLocationChanged(Location myLocation) {
+                if (myLocation != null) {
+                    double lat = myLocation.getLatitude();
+                    double lng = myLocation.getLongitude();
+                    Geocoder geocoder = new Geocoder(getActivity(), Locale.getDefault());
+                    try {
+                        //decode lat and lng into addresses
+                        List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+                        //create location name format with various location parts
+                        String myLocationName = (addresses.get(0).getLocality() == null ? "" : addresses.get(0).getLocality() + ", ") +
+                                (addresses.get(0).getAdminArea() == null ? "" : addresses.get(0).getAdminArea() + ", ") +
+                                (addresses.get(0).getCountryName() == null ? "" : addresses.get(0).getCountryName());
+                        //split up the final location string
+                        String[] myLocNameSplit = myLocationName.split(",");
+                        //because the string will definitely have a part, save the default part as variable in final locname
+                        String myFinalLocName = myLocNameSplit[0].trim();
+
+                        //loop through location parts to check for duplicates
+                        for (String myLocNamePart : myLocNameSplit) {
+                            //skip location part if duplicate
+                            if (!myLocNameSplit[0].trim().equalsIgnoreCase(myLocNamePart.trim())) {
+                                //add location part into final locname string if not a duplicate
+                                myFinalLocName += ", " + myLocNamePart.trim();
+                            }
+
+                        }
+                        location.setText(myFinalLocName);
+                    } catch (IOException e) {
+                        Toast.makeText(getActivity(), "Error getting location!", Toast.LENGTH_LONG).show();
+                        location.setText("UNKNOWN");
+                    }
+                }
+            }
+
+            public void onProviderDisabled(String provider) {
+                Toast.makeText(getActivity(),"Error: Please enable location features!", Toast.LENGTH_LONG).show();
+            }
+
+            public void onProviderEnabled(String provider) { }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) { }
+        };
+
+
+
         noteId = getShownIndex();
-        if(noteId>0){
+        if (noteId > 0) {
+            //edit mode
             TMANote note = mydb.getNoteById(noteId);
             title.setText(note.getTitle());
             content.setText(note.getContent());
             dateTextView.setText(note.getDate());
             timeTextView.setText(note.getTime());
             location.setText(note.getLocation());
-            if(note.getImage()!=null){
+            if (note.getImage() != null) {
                 imvBanner.setImageBitmap(note.getImage());
             }
+        } else {
+            //add mode
+            //check if app has permission,
+            if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            {
+                //if permission not available,
+
+
+                //request for permissions
+                requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+                return;
+            }
+            else{
+                //if permission available, update latest location
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, locationListener);
+            }
+
+
+
         }
 
+        //on click location, will prompt user if a location refresh is required
+        location.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setTitle("Update Location");
+                builder.setMessage("Do you want to update your location?");
+                builder.setPositiveButton("UPDATE", new DialogInterface.OnClickListener() {
 
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        //check if app has permission,
+                        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+                        {
+                            //if permission not available,
+
+
+                            //request for permissions
+                            requestPermissions(new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+                            return;
+                        }
+                        else{
+                            //if permission available, update latest location
+                            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, locationListener);
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                builder.setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Do nothing
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
 
 
         mChangePhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-            View myPopupView = LayoutInflater.from(getActivity()).inflate(R.layout.popup_photo_choice_layout, null, false);
-            puwPhotoChoice = new PopupWindow(
-                myPopupView,view.getRootView().findViewById(R.id.myLinear).getWidth(),
-                view.getRootView().findViewById(R.id.myLinear).getHeight(),
-                true
-            );
-            puwPhotoChoice.showAtLocation(view.getRootView().findViewById(R.id.myLinear), Gravity.CENTER, 0, 0);
-            View popContainer = (View) puwPhotoChoice.getContentView().getParent();
-            WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
-            WindowManager.LayoutParams p = (WindowManager.LayoutParams) popContainer.getLayoutParams();
-            p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
-            p.dimAmount = 0.6f;
-            wm.updateViewLayout(popContainer, p);
-
-            imvCamera = myPopupView.findViewById(R.id.imvCamera);
-            imvGallery = myPopupView.findViewById(R.id.imvGallery);
-
-            imvCamera.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                if (checkCameraHardware(getContext())){
-                    dispatchTakePictureIntent();
-                }
-                //close popup after setting image after 1s
-                new android.os.Handler().postDelayed(
-                    new Runnable() {
-                        public void run() {
-                            puwPhotoChoice.dismiss();
-                        }
-                    }, 1000
+                View myPopupView = LayoutInflater.from(getActivity()).inflate(R.layout.popup_photo_choice_layout, null, false);
+                puwPhotoChoice = new PopupWindow(
+                        myPopupView, view.getRootView().findViewById(R.id.myLinear).getWidth(),
+                        view.getRootView().findViewById(R.id.myLinear).getHeight(),
+                        true
                 );
-                }
-            });
+                puwPhotoChoice.showAtLocation(view.getRootView().findViewById(R.id.myLinear), Gravity.CENTER, 0, 0);
+                View popContainer = (View) puwPhotoChoice.getContentView().getParent();
+                WindowManager wm = (WindowManager) getActivity().getSystemService(Context.WINDOW_SERVICE);
+                WindowManager.LayoutParams p = (WindowManager.LayoutParams) popContainer.getLayoutParams();
+                p.flags |= WindowManager.LayoutParams.FLAG_DIM_BEHIND;
+                p.dimAmount = 0.6f;
+                wm.updateViewLayout(popContainer, p);
 
-            imvGallery.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_REQUEST_CODE);
-                //close popup after setting image after 1s
-                new android.os.Handler().postDelayed(
-                    new Runnable() {
-                        public void run() {
-                            puwPhotoChoice.dismiss();
+                imvCamera = myPopupView.findViewById(R.id.imvCamera);
+                imvGallery = myPopupView.findViewById(R.id.imvGallery);
+
+                imvCamera.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (checkCameraHardware(getContext())) {
+                            dispatchTakePictureIntent();
                         }
-                    }, 1000
-                );
-                }
-            });
+                        //close popup after setting image after 1s
+                        new android.os.Handler().postDelayed(
+                                new Runnable() {
+                                    public void run() {
+                                        puwPhotoChoice.dismiss();
+                                    }
+                                }, 1000
+                        );
+                    }
+                });
+
+                imvGallery.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent();
+                        intent.setType("image/*");
+                        intent.setAction(Intent.ACTION_GET_CONTENT);
+                        startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_REQUEST_CODE);
+                        //close popup after setting image after 1s
+                        new android.os.Handler().postDelayed(
+                                new Runnable() {
+                                    public void run() {
+                                        puwPhotoChoice.dismiss();
+                                    }
+                                }, 1000
+                        );
+                    }
+                });
             }
         });
 
-
+//TODO: saving location and image
         mSaveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 saveNote();
             }
         });
-
-
 
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
@@ -243,7 +374,7 @@ public class NoteDetailsFragment extends Fragment {
         final TimePickerDialog.OnTimeSetListener time = new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
-                updateSelectedTime(hourOfDay,minute);
+                updateSelectedTime(hourOfDay, minute);
             }
         };
 
@@ -266,11 +397,30 @@ public class NoteDetailsFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
-                new TimePickerDialog(getContext(),time,myCalendar.get(Calendar.HOUR_OF_DAY),myCalendar.get(Calendar.MINUTE),true).show();
+                new TimePickerDialog(getContext(), time, myCalendar.get(Calendar.HOUR_OF_DAY), myCalendar.get(Calendar.MINUTE), true).show();
             }
         };
         timeTextView.setOnClickListener(selectTime);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+
+        // If request is cancelled the result arrays are empty.
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED &&
+            ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+        {
+            //permission granted
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 5, locationListener);
+
+        } else {
+            // permission denied
+            Toast.makeText(getActivity(), "ERROR: Permission denied to location access", Toast.LENGTH_SHORT).show();
+            location.setText("UNKNOWN");
+        }
+        return;
+    }
+
 
     private void updateSelectedTime(int hour, int minute) {
         String hourStr = hour<10? "0"+hour : ""+hour;
@@ -310,7 +460,7 @@ public class NoteDetailsFragment extends Fragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
 
-        builder.setMessage(R.string.deleteNote)
+        builder.setMessage("Are you sure you want to delete this note?")
                 .setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         mydb.FlagDeleteRow(getShownIndex());
@@ -325,7 +475,7 @@ public class NoteDetailsFragment extends Fragment {
                     }
                 });
         AlertDialog d = builder.create();
-        d.setTitle("Are you sure?");
+        d.setTitle(R.string.deleteNote);
         d.show();
 
     }
